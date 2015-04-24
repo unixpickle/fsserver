1111583137ab53495fc84823e49ff6534c1d99c6
Alex Nichol <unixpickle@gmail.com>
2015-04-24 15:50:58 -0400

    parent directory needs trailing slash

--- lib/util.go
+++ lib/util.go
@@ -6,5 +6,9 @@ func PathParent(p string) string {
 	if len(p) > 0 && p[len(p)-1] == '/' {
 		p = p[0 : len(p)-1]
 	}
-	return path.Dir(p)
+	parent := path.Dir(p)
+	if len(parent) > 0 && parent[len(parent)-1] != '/' {
+		return parent + "/"
+	}
+	return parent
 }
