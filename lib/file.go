455449735765bd2d9dc7518798c240650526c1cc
Alex Nichol <unixpickle@gmail.com>
2017-06-29 12:45:49 -0400

    use http.ServeContent to support Range & other headers

--- lib/file.go
+++ lib/file.go
@@ -1,11 +1,8 @@
 package fsserver
 
 import (
-	"io"
-	"mime"
 	"net/http"
 	"path"
-	"strconv"
 )
 
 func ServeFile(w http.ResponseWriter, r *http.Request, f http.File) {
@@ -14,12 +11,5 @@ func ServeFile(w http.ResponseWriter, r *http.Request, f http.File) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	ext := path.Ext(r.URL.Path)
-	mimeType := mime.TypeByExtension(ext)
-	if mimeType != "" {
-		w.Header().Set("Content-Type", mimeType)
-	}
-	w.Header().Set("Content-Length", strconv.FormatInt(stats.Size(), 10))
-	io.Copy(w, f)
+	http.ServeContent(w, r, path.Base(r.URL.Path), stats.ModTime(), f)
 }
